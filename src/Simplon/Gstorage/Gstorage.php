a300b1078a8b56f0110cf25cb03c8eadad2a7921
Tino Ehrich <tino@bigpun.me>
2016-04-12 18:31:18 +0200

    renamed class

---
+++ src/Simplon/Gstorage/Gstorage.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Simplon\Gstorage;
+
+use Simplon\Gstorage\Auth\ServerAccountCredentialsInterface;
+use Simplon\Gstorage\Data\ObjectData;
+use Simplon\Gstorage\Data\UploadData;
+
+/**
+ * Class Gstorage
+ * @package Simplon\Gstorage
+ */
+class Gstorage
+{
+    /**
+     * @link https://developers.google.com/identity/protocols/googlescopes#autoscalerv1beta2
+     */
+    const SCOPE = 'https://www.googleapis.com/auth/devstorage.read_write';
+
+    /**
+     * @var ServerAccountCredentialsInterface
+     */
+    private $credentials;
+
+    /**
+     * @var \Google_Client
+     */
+    private $client;
+
+    /**
+     * @var \Google_Service_Storage
+     */
+    private $storage;
+
+    /**
+     * @param ServerAccountCredentialsInterface $credentials
+     */
+    public function __construct(ServerAccountCredentialsInterface $credentials)
+    {
+        $this->credentials = $credentials;
+    }
+
+    /**
+     * @param UploadData $data
+     *
+     * @return null|ObjectData
+     */
+    public function upload(UploadData $data)
+    {
+        $obj = new \Google_Service_Storage_StorageObject();
+        $obj->setName($data->getFileName());
+
+        $options = [
+            'name'          => $data->getFileName(),
+            'data'          => $data->getBlob(),
+            'uploadType'    => 'media',
+            'mimeType'      => $data->getMimeType(),
+            'predefinedAcl' => $data->isPublic() ? 'publicread' : null,
+        ];
+
+        $obj = $this->getStorage()->objects->insert(
+            $data->getBucket(), $obj, $options
+        );
+
+        if ($obj)
+        {
+            return new ObjectData($data->getBucket(), $obj->getName());
+        }
+
+        return null;
+    }
+
+    /**
+     * @param ObjectData $data
+     *
+     * @return bool
+     */
+    public function delete(ObjectData $data)
+    {
+        $response = $this->getStorage()->objects->delete($data->getBucket(), $data->getFileName());
+
+        return $response === null;
+    }
+
+    /**
+     * @return \Google_Service_Storage
+     */
+    private function getStorage()
+    {
+        if ($this->storage === null)
+        {
+            $this->storage = new \Google_Service_Storage($this->getClient());
+        }
+
+        return $this->storage;
+    }
+
+    /**
+     * @return \Google_Auth_AssertionCredentials
+     */
+    private function getCredentials()
+    {
+        return new \Google_Auth_AssertionCredentials($this->credentials->getClientEmail(), [self::SCOPE], $this->credentials->getPrivateKey());
+    }
+
+    /**
+     * @return \Google_Client
+     */
+    private function getClient()
+    {
+        if ($this->client === null)
+        {
+            $this->client = new \Google_Client();
+            $this->client->setAssertionCredentials($this->getCredentials());
+        }
+
+        return $this->client;
+    }
+}
